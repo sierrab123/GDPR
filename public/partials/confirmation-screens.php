2eae6d5bb281519a6b1b450b4fedb34738facbc4
Fernando Claussen <fernandoclaussen@gmail.com>
2018-05-24 17:45:43 -0400

    Cleaning files with phpcs

--- public/partials/confirmation-screens.php
+++ public/partials/confirmation-screens.php
@@ -13,34 +13,35 @@
 
 <?php
 $title = '';
-$text = '';
-if ( isset( $_GET['notify'] ) && $_GET['notify'] ) : ?>
+$text  = '';
+if ( isset( $_GET['notify'] ) && sanitize_text_field( wp_unslash( $_GET['notify'] ) ) ) : // WPCS: input var ok, CSRF ok.
+?>
 	<?php
-	if ( isset( $_GET['user-deleted'] ) ) {
+	if ( isset( $_GET['user-deleted'] ) ) { // WPCS: input var ok, CSRF ok.
 		$title = __( 'Your account', 'gdpr' );
-		if ( $_GET['user-deleted'] ) {
+		if ( sanitize_text_field( wp_unslash( $_GET['user-deleted'] ) ) ) { // WPCS: input var ok, CSRF ok.
 			$text = __( 'Your account has been closed. We are sorry to see you go.', 'gdpr' );
 		} else {
 			$text = __( 'Your request has been received and is being reviewed. You will receive an email when we are done.', 'gdpr' );
 		}
 	}
-	if ( isset( $_GET['request-confirmed'] ) && $_GET['request-confirmed'] ) {
+	if ( isset( $_GET['request-confirmed'] ) && sanitize_text_field( wp_unslash( $_GET['request-confirmed'] ) ) ) { // WPCS: input var ok, CSRF ok.
 		$title = __( 'Request Received', 'gdpr' );
 		$text  = __( 'Your request has been received. We will be in touch soon.', 'gdpr' );
 	}
-	if ( isset( $_GET['malformed-confirmation-link'] ) && $_GET['malformed-confirmation-link'] ) {
+	if ( isset( $_GET['malformed-confirmation-link'] ) && sanitize_text_field( wp_unslash( $_GET['malformed-confirmation-link'] ) ) ) { // WPCS: input var ok, CSRF ok.
 		$title = __( 'Error!', 'gdpr' );
 		$text  = __( 'Malformed request confirmation link!', 'gdpr' );
 	}
-	if ( isset( $_GET['user-not-found'] ) && $_GET['user-not-found'] ) {
+	if ( isset( $_GET['user-not-found'] ) && sanitize_text_field( wp_unslash( $_GET['user-not-found'] ) ) ) { // WPCS: input var ok, CSRF ok.
 		$title = __( 'Error!', 'gdpr' );
 		$text  = __( 'User not found.', 'gdpr' );
 	}
-	if ( isset( $_GET['request-key-not-found'] ) && $_GET['request-key-not-found'] ) {
+	if ( isset( $_GET['request-key-not-found'] ) && sanitize_text_field( wp_unslash( $_GET['request-key-not-found'] ) ) ) { // WPCS: input var ok, CSRF ok.
 		$title = __( 'Error!', 'gdpr' );
 		$text  = __( 'We could not confirm the request key. It may be expired.', 'gdpr' );
 	}
-	if ( isset( $_GET['request-key-not-match'] ) && $_GET['request-key-not-match'] ) {
+	if ( isset( $_GET['request-key-not-match'] ) && sanitize_text_field( wp_unslash( $_GET['request-key-not-match'] ) ) ) { // WPCS: input var ok, CSRF ok.
 		$title = __( 'Error!', 'gdpr' );
 		$text  = __( 'The key used does not match the request key we have stored.', 'gdpr' );
 	}
@@ -58,7 +59,7 @@ if ( isset( $_GET['notify'] ) && $_GET['notify'] ) : ?>
 			<p><?php echo esc_html( $text ); ?></p>
 		</div>
 		<footer>
-			<button class="gdpr-ok"><?php esc_html_e( 'OK', 'gdpr' ) ?></button>
+			<button class="gdpr-ok"><?php esc_html_e( 'OK', 'gdpr' ); ?></button>
 		</footer>
 	</div>
 </div>
